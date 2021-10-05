1a5e8db5e217b5cd9117e85fe1280981c3690dbe
TanQin <tagtan@qq.com>
2021-10-05 21:13:07 +0800

    feat: 路由懒加载

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import Tabbar from './components/tabbar'
 /* 1.0 引入路由组件 */
 import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
-/* 2.0 导入组件 */
-import Home from './components/home/Index.js'
-import House from './components/house/Index.js'
-import Info from './components/info/Index.js'
-import My from './components/my/Index.js'
+/* 2.0 导入组件 - 组件懒加载 */
+const Home = React.lazy(() => import('./components/home/Index.js')) 
+const House = React.lazy(() => import('./components/house/Index.js')) 
+const Info = React.lazy(() => import('./components/info/Index.js')) 
+const My = React.lazy(() => import('./components/my/Index.js')) 
 class App extends Component {
   render() {
     return (
       // 根节点
-      <Router>
-        {/* 3.0 配置路由 */}
+      <React.Suspense fallback={<div>loading...</div>}>
+        <Router>
+        {/* 3.0 配置路由 - 路由懒加载 */}
         <Route path="/" render={() => <Redirect to="/index"></Redirect>}></Route>
         <Route path="/index" render={(props) => <Tabbar {...props} type="index"><Home></Home></Tabbar>}></Route>
         <Route path="/house" render={(props) => <Tabbar {...props} type="house"><House></House></Tabbar>}></Route>
         <Route path="/info" render={(props) => <Tabbar {...props} type="info"><Info></Info></Tabbar>}></Route>
         <Route path="/my" render={(props) => <Tabbar {...props} type="my"><My></My></Tabbar>}></Route>
       </Router>
+      </React.Suspense>
     );
   }
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,7 +4,7 @@ class Index extends Component {
     render() {
         return (
             <div>
-                <h1>首页</h1>
+                首页
             </div>
         );
     }
